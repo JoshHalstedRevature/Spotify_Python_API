540c444d3e0db73c37d83979401a0b1a25d4cff7
n0hb0dy <36122937+n0hb0dy@users.noreply.github.com>
2021-12-30 16:51:48 -0500

    functioning search engine

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 #program to ..
 #Python 2.7.5 in HortonWorks
 #import logging
-from pyspark.sql import SparkSession
+#from pyspark.sql import SparkSession
 import urllib, httplib
 
 class SpotifyRequests:
      """Handles communicating with the Spotify API"""
      __SpotifyConnect = httplib.HTTPSConnection('api.spotify.com')
-     _OAuth_Token = 'BQDhq7QX6_bacG9wwbUYYaGArfb3fenMSNAbeAmvDFDapZnZZCe3TOsy4mX_3AMUeRG2Ebocesfi8iTG0D9qPScAYBqk_QGKh_7NpL1iXeuasb_V9xO_cDX465Um1O4UlL37rHcaKM5tIMdLyHrSOO_RAAJHuW0N45JXHC_n' #expires after a day
+     _OAuth_Token = 'BQB5b9QK_nn78YmAJfZEnRz2xb2L7pjE4x6ieE0iUl6gcBLvUULRt0XO-bK6-J0s6IlTdlI9uMeVPouWP5d9xZssu3yEpElRZJ35ER8aNM2b00Z9h80Lrdu-y_0oIuK4sAoLcysqiFaD4pJ9cZ012ucXMC_CcIcdbHKwuf8P' #expires after a day
      _Client_ID = '4ffccca0d17a4f70b049aab86f2cc0bb'
      _Client_Secret = '5e2d61615b964c42a14d07e1c61d110c'
      _headers = {
@@ -18,36 +18,60 @@ class SpotifyRequests:
      }
 
      def __init__(self):
+          """Constructor"""
           pass
 
+     def __del__(self):
+          """Destructor"""
+          self.__SpotifyConnect.close()
+
      def json(self, contentStr):
+          """To parse the API return content in to simple strings"""
           pass
 
      def renew_OAuth_token(self):
+          """Will acquire or renew an OAuth token programatically"""
           pass
 
-     def search(self, q, rType, market = '', limit = 0, offset = 5, include_external = ''):
-          
+     def search(self, q, rType, market = '', limit = -1, offset = -1, include_external = ''):
+          """Will use the Spotify API's search URL to search for keywords in a category"""
+          params = urllib.urlencode({
+               'q' : q,
+               'type' : rType
+          })
+
+          if market != '': params += '&' + urllib.urlencode({ 'market' : market})
+          if limit != -1: params += '&' + urllib.urlencode({ 'limit' : limit})
+          if offset != -1: params += '&' + urllib.urlencode({ 'offset' : offset})
+          if include_external != '': params += '&' + urllib.urlencode({ 'include_external' : include_external})     
+
           self.__SpotifyConnect.request(
                'GET',
-               url = '/v1/search?q=kanye&type=artist',
+               url = '/v1/search?{}'.format(params),
                headers = self._headers
           )
+
           response = self.__SpotifyConnect.getresponse()
-          #print(response.status, response.reason) #replace with logging
-          self.__SpotifyConnect.close()
-          return response.read()
+          #print('api.spotify.com/v1/search?{}'.format(params))                                     #replace with logging 
+          print(response.status, response.reason)                                                   #replace with logging
+          
+          if response.status == 200:
+               return response.read() 
+          else:
+               return str(response.status)
 
 def init():
      """The beginning setup of the main function"""
      global spark 
-     spark = SparkSession.builder\
-          .appName('SparkByExamples.com')\
+     spark = SparkSession.builder \
+          .appName('SparkByExamples.com') \
           .getOrCreate()
 
 def main():
-     init()
-     spark.stop()
+     #init()
+     print(SpotifyRequests().search("kanye west", "artist")) 
+
+     #spark.stop()
 
 if __name__ == '__main__':
      main()
