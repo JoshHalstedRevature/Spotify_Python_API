1041be66af0dfebb6e5dd86562baa0afa72d412f
n0hb0dy <36122937+n0hb0dy@users.noreply.github.com>
2021-12-30 11:47:52 -0500

    remade with httplib to support Py2.7.5

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 #program to ..
-#Python 3.9.6
-import pyspark
-import requests
+#Python 2.7.5 in HortonWorks
+#import pyspark
+#import requests
+import urllib, httplib
 
 # Globals for now
-OAuth_Token = 'BQAagdfCBY0GqVjL8wixXIBcoJeVvjo7DUj3xMAbgnxltPI3fIB5OlzkkituDrcFLSt14h-KO47jllQpFDUJHTybXzxpcg35raiga0IMvhBmkjXYS9Mt3TPnDVDwAu-2pz-wYINSBdOEMaXuDuuGWTJIVpiywgd7jfviaeFv'
+OAuth_Token = 'BQDhq7QX6_bacG9wwbUYYaGArfb3fenMSNAbeAmvDFDapZnZZCe3TOsy4mX_3AMUeRG2Ebocesfi8iTG0D9qPScAYBqk_QGKh_7NpL1iXeuasb_V9xO_cDX465Um1O4UlL37rHcaKM5tIMdLyHrSOO_RAAJHuW0N45JXHC_n' #expires after a day
 Client_ID = '4ffccca0d17a4f70b049aab86f2cc0bb'
 Client_Secret = '5e2d61615b964c42a14d07e1c61d110c'
 
@@ -15,18 +16,28 @@ def init():
      
      #searching for kanye
      headers = {
+          'Content-Length' : 0,         # Needs to be set to 0
           'Accept' : 'application/json',
           'Content-Type' : 'application/json',
-          'Authorization' : f'Bearer {OAuth_Token}'
+          'Authorization' : 'Bearer {}'.format(OAuth_Token)
      }
 
-     content = requests.get(
-          'https://api.spotify.com/v1/search?q=kanye&type=artist',
+     connection = httplib.HTTPSConnection('api.spotify.com')
+     
+     connection.request(
+          'GET',
+          url = '/v1/search?q=kanye&type=artist',
           headers = headers
      )
 
-     print(content.json())
-     
+     print(connection)
+
+     response = connection.getresponse()
+     print(response.status, response.reason)
+     data = response.read()
+     print(data)
+
+     connection.close()
 
 def main():
      init()
