79ec1981a0c48233c459679a5607c2c55b30ff13
n0hb0dy <36122937+n0hb0dy@users.noreply.github.com>
2022-01-03 13:38:07 -0500

    basic authorization (passing_credentials)

--- src/main.py
+++ src/main.py
@@ -2,20 +2,15 @@
 #Python 2.7.5 in HortonWorks
 #import logging
 #from pyspark.sql import SparkSession
-import urllib, httplib
+import urllib, httplib, json
+import base64
 
 class SpotifyRequests:
      """Handles communicating with the Spotify API"""
      __SpotifyConnect = httplib.HTTPSConnection('api.spotify.com')
-     _OAuth_Token = 'BQB5b9QK_nn78YmAJfZEnRz2xb2L7pjE4x6ieE0iUl6gcBLvUULRt0XO-bK6-J0s6IlTdlI9uMeVPouWP5d9xZssu3yEpElRZJ35ER8aNM2b00Z9h80Lrdu-y_0oIuK4sAoLcysqiFaD4pJ9cZ012ucXMC_CcIcdbHKwuf8P' #expires after a day
+     _access_token = 'BQB5b9QK_nn78YmAJfZEnRz2xb2L7pjE4x6ieE0iUl6gcBLvUULRt0XO-bK6-J0s6IlTdlI9uMeVPouWP5d9xZssu3yEpElRZJ35ER8aNM2b00Z9h80Lrdu-y_0oIuK4sAoLcysqiFaD4pJ9cZ012ucXMC_CcIcdbHKwuf8P' #expires after a day
      _Client_ID = '4ffccca0d17a4f70b049aab86f2cc0bb'
      _Client_Secret = '5e2d61615b964c42a14d07e1c61d110c'
-     _headers = {
-          'Content-Length' : 0,         # Needs to be set to 0 when passing headers
-          'Accept' : 'application/json',
-          'Content-Type' : 'application/json',
-          'Authorization' : 'Bearer {}'.format(_OAuth_Token)
-     }
 
      def __init__(self):
           """Constructor"""
@@ -25,16 +20,54 @@ class SpotifyRequests:
           """Destructor"""
           self.__SpotifyConnect.close()
 
-     def json(self, contentStr):
-          """To parse the API return content in to simple strings"""
-          pass
+     def jsonParsed(self, contentStr):
+          """To parse the API return content into simple strings"""
+          parsedString = json.loads(contentStr) # ??
+          return parsedString
 
-     def renew_OAuth_token(self):
-          """Will acquire or renew an OAuth token programatically"""
-          pass
+     def passing_credentials(self):
+          """For getting an access token (expiration is 3600)"""
+          authConnection = httplib.HTTPSConnection('accounts.spotify.com')
+          
+          message = self._Client_ID + ':' + self._Client_Secret
+#          encoded_message = str(base64.b64encode(message.encode('ascii')))
+          encoded_message = str(base64.b64encode(message))
+
+          params = urllib.urlencode({
+               'grant_type' : 'client_credentials'
+          })
+
+          authHeaders = {
+               'Authorization' : 'Basic ' + encoded_message,
+               'Content-Type' : 'application/x-www-form-urlencoded',
+          }
+
+          authConnection.request(
+               'POST',
+               url = '/api/token',
+               body = params,
+               headers = authHeaders
+          )
+
+          response = authConnection.getresponse()
+
+          print(response.status, response.reason)  
+          
+          if response.status == 200:
+               print(response.read())
+               #self._access_token =  response.read() #need to parse this
+          else:
+               print(response.read())
 
      def search(self, q, rType, market = '', limit = -1, offset = -1, include_external = ''):
           """Will use the Spotify API's search URL to search for keywords in a category"""
+          searchHeaders = {
+               'Content-Length' : 0,                   # no body so content-length should be 0
+               'Accept' : 'application/json',
+               'Content-Type' : 'application/json',
+               'Authorization' : 'Bearer {}'.format(self._access_token)
+          }
+          
           params = urllib.urlencode({
                'q' : q,
                'type' : rType
@@ -48,7 +81,7 @@ class SpotifyRequests:
           self.__SpotifyConnect.request(
                'GET',
                url = '/v1/search?{}'.format(params),
-               headers = self._headers
+               headers = searchHeaders
           )
 
           response = self.__SpotifyConnect.getresponse()
@@ -68,10 +101,10 @@ def init():
           .getOrCreate()
 
 def main():
-     #init()
-     print(SpotifyRequests().search("kanye west", "artist")) 
+     #init() #comment out when testing outside of the VM
+     SpotifyRequests().passing_credentials()
 
-     #spark.stop()
+     #spark.stop()  #comment out when testing outside of the VM
 
 if __name__ == '__main__':
      main()
