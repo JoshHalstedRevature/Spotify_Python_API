311e451716fd6b27b26a67284ba36918a9d5a173
n0hb0dy <36122937+n0hb0dy@users.noreply.github.com>
2022-01-13 15:57:48 -0500

    still working on getting s3 bucket/api calling popular playlists

--- src/main.py
+++ src/main.py
@@ -6,7 +6,6 @@ from spotifyapi import SpotifyRequests
 def main():
      sc = SparkControl() # comment out when testing in VSCode
      SpotifyRequests().passing_credentials()
-     print("At least the package was submitted!!!")
 
 if __name__ == '__main__':
      main()
